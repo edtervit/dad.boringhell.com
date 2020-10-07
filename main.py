1ea913fca900ef47eb7f1e60ced5ffb0767688a8
Ed Tervit <edtervit@hotmail.co.uk>
2020-10-07 11:38:48 +0100

    Added styling to page.

--- main.py
+++ main.py
@@ -22,26 +22,27 @@ top_html = '''<!DOCTYPE html>
     <h1>Ned's Race Reminder</h1>
     <p> Ticking a box should play a sound 3 minutes before the race starts</p>
     <button class="stop" onclick="alarmSound.pause()">STOP SOUND</BUTTON>
-    
+<div class="container">
     '''
 
 main_html = '''
     
 ''' 
 bot_html = '''
-
+</div>
 <audio id="alarm" src="sounds/crazy_frog.mp3" preload="auto" hidden></audio>
 <script src="ringer.js"></script> 
 </body>
 </html> '''
 
 for key, value in times_and_tracks.items():
+    main_html += "<div class=a_track> \n"
     main_html += "<h2>" + key + "</h2>" + "\n" # for each key (track) make a h2 with its name
     main_html += "  <h3 class='going'>" + value[0] + "</h3>" + "\n" # write first item (going) in the value's list (going+times) 
     main_html += "  <ul class='the_times'>" + "\n"    #create a list opening
     for i in value[1:]: #for each i in list of times create a list item
         main_html += "      <li class ='time'>" + i + "<input type='checkbox' id='" + key + i + "' onclick='checkIfChecked(this)'>" + "</li>" + "\n"
-    main_html += "  </ul>" # closes list
+    main_html += "  </ul> \n</div> \n" # closes list
     
 
 final_html = top_html + main_html + bot_html
